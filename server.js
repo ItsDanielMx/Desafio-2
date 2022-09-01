929444af6d7a0bb79796cade5ef65c210a29a3dc
Daniel Hernandez <danielhernandez09012002@gmail.com>
2022-09-01 13:31:25 -0500

    server

--- server.js
+++ server.js
@@ -1,25 +1,21 @@
 const express = require('express')
-const Manager = require('./manager.js')
-
+const Contenedor = require('./contenedor.js')
+const obtenerProductos = new Contenedor()
 const app = express();
 
 const server = app.listen(8080, () => console.log("Server Up!"));
 server.on("error", (error) => console.log(`Error en el servidor`));
 
-let numeroAleatorio = 0;
-let productoRandom = [];
-const fileSystem = new Manager("./products.json");
-
-const main = async () => {
-  const productos = await fileSystem.getAll();
-
-  app.get("/productos", (req, res) => {
-    res.json(productos);
-  });
-  app.get("/productoRandom", (req, res) => {
-    numeroAleatorio = Math.floor(Math.random() * productos.length) + 1;
-    productoRandom = productos.find((producto) => producto.id === numeroAleatorio);
-    res.json(productoRandom);
-  });
-};
-main();
+app.get('/', (request, response) => {
+    response.send("<h1 style= 'color:blue' align = 'center'>Hola Mundo desde express</h1>");
+    })
+app.get('/productos', (request, response) => {
+    obtenerProductos.getAll().then(result => response.send(result))
+    })
+app.get('/productoRandom', (request, response) => {
+    obtenerProductos.getRandom().then(result => response.send(result))
+   
+    });    
+app.get('/*', (request,response) => {
+    response.status(404,).send("<h1 style = 'color:red' align = 'center'>PAGINA NO ENCONTRADA</h1>")
+    })    
